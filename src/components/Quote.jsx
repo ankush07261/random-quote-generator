6ccb6fc981dc9abbce1124c8dabcf41a61de801f
Ankush Hegde <ankushhegde30@gmail.com>
2023-11-23 21:04:43 +0530

    first-commit

---
+++ src/components/Quote.jsx
@@ -0,0 +1,40 @@
+import './css/styles.css'
+import React, { useEffect, useState } from 'react'
+
+function Quote() {
+    const [data, setData] = useState([]);
+    const [quote, setQuote] = useState([]);
+
+    useEffect(()=>{
+        fetch("https://type.fit/api/quotes")
+        .then(res=>res.json())
+        
+        .then((respone)=>setData(respone));
+        // .catch((err)=>console.error(err))
+       
+    },[setData]);
+     
+    console.log(data);
+
+    let gen = () => {
+        let rno = Math.floor(Math.random() * 15);
+        let eachq = [data[rno]]; 
+        setQuote(eachq);
+        return eachq;    
+    }
+  return (
+      <div className='container'>
+          <h2>Quote :</h2>
+          {
+            quote?.map((quote, index) => (
+              <div className='quote' key={index}>
+                <h3>{quote?.text}</h3>
+              </div>
+            ))   
+          }
+          <button onClick={gen}>generate</button>   
+    </div>
+  )
+}
+
+export default Quote
